7a50bbb7bad64f924f1dd69165f5868d40d727d4
masuyama-2023 <sho-funmatsuryokucha@outlook.jp>
2023-11-27 11:09:52 +0900

    発狂寸前　全然うまくいかないぉ・・・　day11.27

--- akb_teamD/src/main/java/com/example/akb_teamD/app/controller/GlobalMoveController.java
+++ akb_teamD/src/main/java/com/example/akb_teamD/app/controller/GlobalMoveController.java
@@ -10,6 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+
 import java.util.List;
 import java.util.Map;
 
@@ -25,7 +34,24 @@ import java.util.Map;
 @Controller
 public class GlobalMoveController
 {
+    // データベースのURL
+    private static final String URL = "jdbc:postgresql://localhost:5432/springboot";
 
+    // データベースにアクセスするユーザー
+    private static final String USER = "postgres";
+
+    // データベースにアクセスするユーザーのパスワード
+    private static final String PASSWORD = "postgres";
+
+    /**
+     * データベースの接続を取得する
+     * @return
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -61,11 +87,52 @@ public class GlobalMoveController
 
 
     @GetMapping("/user_contact_address")
-    public String address() {
+    public String address() throws ClassNotFoundException, SQLException{
         //ここの　String sql = 以下の文をちゃんと書ければ動くはず？現状は仮置き。
         //idとnameは現在ログインしてるユーザー情報を取得。残りの３つは入力欄に入力した内容を入れる。
+        // ？？？は全部仮置き。当然だが仮置きなので動かない。
         //String sql = "INSERT INTO address_table (id,name,phone,mail,other) VALUES (？？？,'？？？','？？？','？？？','？？？')";
         //jdbcTemplate.update(sql);
+
+        int result = 0;
+
+
+        String sql = "INSERT INTO address_table (id,name,phone,mail,other) VALUES(?, ?, ?, ?, ?);";
+        //String sql = "INSERT INTO address_table (id,name,phone,mail,other) VALUES(2,'test1','a','b','c');";
+
+/*
+
+        String phone = request.getParameter("phone");
+        String mail = Integer.parseInt(request.getParameter("mail"));
+        String other = Integer.parseInt(request.getParameter("remark"));
+*/
+
+        try (Connection con = getConnection();PreparedStatement pstmt = con.prepareStatement(sql);) {
+            // パラメータの設定
+/*
+            pstmt.setInt(1, 1);
+            pstmt.setString(2,"test1");
+            pstmt.setString(3, phone);
+            pstmt.setString(4, mail);
+            pstmt.setString(5, other);
+*/
+
+
+            //全然うまくいかないのでベタ打ちテスト
+            //データベースにデータが入るのにページが開けない謎。
+            pstmt.setInt(1, 3);
+            pstmt.setString(2,"test3");
+            pstmt.setString(3, "000-0000-0001");
+            pstmt.setString(4, "aiueo@gmail.com");
+            pstmt.setString(5, "あいうえお");
+
+            // SQLの実行
+            result = pstmt.executeUpdate();
+
+        }
+
+
+        jdbcTemplate.update(sql);
         return "user_contact_address";
     }
 
